cae1212ca7d87f0cffdba61a81eff3e27659e3a2
wdtteam <vkontakte362@gmail.com>
2016-07-04 13:43:15 +0500

    Boot-Update

--- gendesc.php
+++ gendesc.php
@@ -4,4 +4,4 @@ $field2  = $_POST['field2'];
 $field3  = $_POST['field3'];
 
 $data = "$field1 $field2 $field3";
-file_put_contents('desc.txt', $data, FILE_APPEND);
+file_put_contents('gen/desc.txt', $data, FILE_APPEND);
