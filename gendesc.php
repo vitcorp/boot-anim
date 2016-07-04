7f46afb59a9faafd9f3582093660650f80ec1585
wdtteam <vkontakte362@gmail.com>
2016-07-04 21:06:45 +0500

    Update

--- gendesc.php
+++ gendesc.php
@@ -3,5 +3,8 @@ $field1  = $_POST['field1'];
 $field2  = $_POST['field2'];
 $field3  = $_POST['field3'];
 
-$data = "$field1 $field2 $field3";
+$data = "$field1 $field2 $field3
+p 1 0 Part0
+p 0 1 Part1
+";
 file_put_contents('gen/desc.txt', $data, FILE_APPEND);
