300f536463312d17b6e3d340f5fcfe89a057c34c
wdtteam <vkontakte362@gmail.com>
2016-07-04 13:15:03 +0500

    Boot-Anim

---
+++ gendesc.php
@@ -0,0 +1,7 @@
+<?php 
+$field1  = $_POST['field1'];
+$field2  = $_POST['field2'];
+$field3  = $_POST['field3'];
+
+$data = "$field1 $field2 $field3";
+file_put_contents('desc.txt', $data, FILE_APPEND);
